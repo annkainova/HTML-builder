3bdb4b65015794c51bbee49ffa5245c6b26a299d
Anna <cainowa@gmail.com>
2024-01-18 20:50:44 +0100

    feat: add 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const readline = require('readline');
+const path = require('path');
+
+let filePath = path.join(__dirname, 'text.txt');
+const newFile = fs.createWriteStream(filePath);
+
+let rl = readline.createInterface(process.stdin, process.stdout);
+rl.setPrompt(`Write some text: \n`);
+rl.prompt();
+rl.on('line', (text) => {
+  if (text.toLowerCase() === 'exit') {
+    console.log('Goodbye!');
+    rl.close();
+  } else {
+    newFile.write(`${text}\n`);
+  }
+});
