01754dadf3c249f0a74086f7ed1d3e2800492c9c
Anna <cainowa@gmail.com>
2024-01-19 23:18:41 +0100

    feat: add 04-copy-directory task

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+const path = require('path');
+
+let folderPath = path.join(__dirname, 'files');
+let folderCopyPath = path.join(__dirname, 'files-copy');
+
+//create folder
+fs.mkdir(folderCopyPath, { recursive: true }, (error) => {
+  if (error) {
+    console.log('Error create folder', error);
+    return;
+  }
+});
+
+// foldres content
+
+fs.readdir(folderPath, { withFileTypes: true }, (error, folderContents) => {
+  if (error) {
+    console.log('Error folder content', error);
+    return;
+  }
+  console.log('folders file: ', folderContents);
+
+  // copy-foldres content
+  fs.readdir(
+    folderCopyPath,
+    { withFileTypes: true },
+    (error, folderCopyContents) => {
+      if (error) {
+        console.log('Error COPY folder content', error);
+        return;
+      }
+      console.log('COPY-folders file: ', folderCopyContents);
+
+      //delete file in copy-folder
+      folderCopyContents.forEach((copyContent) => {
+        if (
+          !folderContents.some((content) => content.name === copyContent.name)
+        ) {
+          const fullCopyPath = path.join(folderCopyPath, copyContent.name);
+          fs.unlink(fullCopyPath, (err) => {
+            if (err) {
+              console.log('Error delete file: ', err);
+            }
+            console.log('Delete file: ', copyContent.name);
+          });
+        }
+      });
+    },
+  );
+
+  //copy and update file
+  folderContents.forEach((content) => {
+    const sourceFile = path.join(folderPath, content.name);
+    const copyFile = path.join(folderCopyPath, content.name);
+
+    fs.copyFile(sourceFile, copyFile, (err) => {
+      if (err) {
+        console.error('Error in copy process:', err);
+        return;
+      }
+      console.log('Copy sucsess!');
+    });
+  });
+});
