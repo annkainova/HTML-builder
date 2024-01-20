bef59e5138eaa644f121177b0e1e6701f4be167a
Anna <cainowa@gmail.com>
2024-01-20 23:30:35 +0100

    feat: add merge styles file to project-dist

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -49,3 +49,50 @@ function replaceContentInTemplate(tagName) {
     newHTML.write(textTemplate);
   });
 }
+
+//02 merge style
+
+let pathFolderCSS = path.join(__dirname, 'styles');
+
+let projectPath = path.join(__dirname, 'project-dist/style.css');
+const styleFile = fs.createWriteStream(projectPath);
+
+//content folder Styles
+fs.readdir(pathFolderCSS, { withFileTypes: true }, (error, folderContents) => {
+  if (error) {
+    console.log('Error folder content', error);
+    return;
+  }
+
+  // detect file
+  let cssFiles = folderContents
+    .filter(
+      (content) => content.isFile() && path.extname(content.name) === '.css',
+    )
+    .map((content) => path.join(pathFolderCSS, content.name));
+
+  readAndCombineCss(cssFiles);
+});
+
+//read and merge
+function readAndCombineCss(files) {
+  files.forEach((file) => {
+    let readFile = fs.createReadStream(file, 'utf-8');
+    readFile.on('data', (chunk) => {
+      styleFile.write(chunk);
+    });
+    readFile.on('end', () => {
+      styleFile.end();
+    });
+  });
+}
+
+styleFile.on('finish', () => {
+  console.log('Merge sucsess!');
+});
+
+styleFile.on('error', (error) => {
+  console.error(`Error in merge:' ${error.message}`);
+});
+
+
