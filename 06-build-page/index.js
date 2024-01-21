982c74e423f658e741d64fda3ec9cf3790110317
Anna <cainowa@gmail.com>
2024-01-21 18:38:18 +0100

    feat: add funq copy assets folder

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -95,4 +95,87 @@ styleFile.on('error', (error) => {
   console.error(`Error in merge:' ${error.message}`);
 });
 
+03 copy assets file
 
+let folderPath = path.join(__dirname, 'assets');
+let folderCopyPath = path.join(__dirname, 'project-dist/assets');
+
+createFolder(folderCopyPath);
+createFolder(folderCopyPath, '/fonts');
+createFolder(folderCopyPath, '/img');
+createFolder(folderCopyPath, '/svg');
+
+function createFolder(pathFromCopy, addPath = '') {
+  fs.mkdir(`${pathFromCopy}${addPath}`, { recursive: true }, (error) => {
+    if (error) {
+      console.log('Error create folder', error);
+      return;
+    }
+  });
+}
+
+copyFolderContent(folderPath, folderCopyPath, '/fonts');
+copyFolderContent(folderPath, folderCopyPath, '/img');
+copyFolderContent(folderPath, folderCopyPath, '/svg');
+
+function copyFolderContent(pathFromCopy, pathToCopy, addPath = '') {
+  // foldres content
+  fs.readdir(
+    `${pathFromCopy}${addPath}`,
+    { withFileTypes: true },
+    (error, folderContents) => {
+      if (error) {
+        console.log('Error folder content', error);
+        return;
+      }
+      console.log('folders file: ', folderContents);
+
+      // where to copy
+      fs.readdir(
+        `${pathToCopy}${addPath}`,
+        { withFileTypes: true },
+        (error, folderCopyContents) => {
+          if (error) {
+            console.log('Error COPY folder content', error);
+            return;
+          }
+          console.log('COPY-folders file: ', folderCopyContents);
+
+          //delete file in copy-folder
+          folderCopyContents.forEach((copyContent) => {
+            if (
+              !folderContents.some(
+                (content) => content.name === copyContent.name,
+              )
+            ) {
+              const fullCopyPath = path.join(
+                `${pathToCopy}${addPath}`,
+                copyContent.name,
+              );
+              fs.unlink(fullCopyPath, (err) => {
+                if (err) {
+                  console.log('Error delete file: ', err);
+                }
+                console.log('Delete file: ', copyContent.name);
+              });
+            }
+          });
+        },
+      );
+
+      //copy and update file
+      folderContents.forEach((content) => {
+        const sourceFile = path.join(`${pathFromCopy}${addPath}`, content.name);
+        const copyFile = path.join(`${pathToCopy}${addPath}`, content.name);
+
+        fs.copyFile(sourceFile, copyFile, (err) => {
+          if (err) {
+            console.error('Error in copy process:', err);
+            return;
+          }
+          console.log('Copy sucsess!');
+        });
+      });
+    },
+  );
+}
